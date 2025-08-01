9a9fc7540b12eb04ed4fa3cddbcb2d86b7a632e6
marshall800900 <marshall800900@gmail.com>
2025-08-01 09:08:28 +0800

    调整

--- application/config.php
+++ application/config.php
@@ -276,9 +276,9 @@ return [
         //登录失败超过10次则1天后重试
         'login_failure_retry'   => true,
         //是否同一账号同一时间只能在一个地方登录
-        'login_unique'          => false,
+        'login_unique'          => true,
         //是否开启IP变动检测
-        'loginip_check'         => true,
+        'loginip_check'         => false,
         //登录页默认背景图
         'login_background'      => "",
         //是否启用多级菜单导航
